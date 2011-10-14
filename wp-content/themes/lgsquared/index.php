d2af7620e4c9756e8c2247b9c150bee7855fd3e1
Jarad Comingdeer <jarad@jcomingdeer.com>
2011-10-14 09:16:41 -0700

    added a link to the hompage banner

--- wp-content/themes/lgsquared/index.php
+++ wp-content/themes/lgsquared/index.php
@@ -13,7 +13,7 @@ get_header(); ?>
 			<script src="<?php bloginfo('template_directory');?>/slider/jquery.nivo.slider.pack.js" type="text/javascript"></script>
 			
 			<div id="slider" class="nivoSlider">
-				<img src="images/Banner6.jpg" alt="" />
+				<a href="http://windigosurfboards.com/?p=615"><img src="images/Banner6.jpg" alt="Congratulations Grant Ellis" /></a>
 				<!-- <a href="http://store.windigosurfboards.com/product/cash"><img src="images/Banner5.jpg" alt="" /></a> -->
 			</div>
 			<div id="htmlcaption" class="nivo-html-caption">
